72fedd634512ab5366ea373973629c42896c16cc
Thomas Vu <thomas.vu@ucalgary.ca>
2017-10-26 17:23:53 -0600

    Empty class

---
+++ Inspector.java
@@ -0,0 +1,20 @@
+/*==========================================================================
+File: Inspector.java
+Purpose: Object inspector for the Asst2TestDriver
+
+Location: University of Calgary, Alberta, Canada
+Original code by: Jordan Kidney
+Adapted code by: Thomas Vu
+Created on: Oct 26, 2017
+Last Updated: Oct 26, 2017
+========================================================================*/
+
+import java.util.*;
+import java.lang.reflect.*;
+
+public class Inspector
+{
+	public Inspector() { }
+	
+	
+}
