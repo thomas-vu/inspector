3219e82a6c3d13e1a8fec4ab8f3a505f9d170f01
Thomas Vu <thomas.vu@ucalgary.ca>
2017-10-27 14:50:40 -0600

    Implemented 'inspectMethods' Unit Test

--- UnitTesting.java
+++ UnitTesting.java
@@ -21,4 +21,52 @@ public class UnitTesting {
         		" - java.lang.Runnable\n\n", actualOutput);
     }
 
+	@Test
+    public void inspectMethodsTest() throws Exception {
+        OutputStream os = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(os));
+        
+        Inspector inspector = new Inspector();
+        ClassT clT = new ClassT();
+        inspector.inspectMethods(clT.getClass(), true);
+
+        String actualOutput = os.toString();
+        assertEquals("Methods: \n" + 
+        		"    Name: getVal\n" + 
+        		"    Exceptions thrown: \n" + 
+        		"        None\n" + 
+        		"    Parameter types: \n" + 
+        		"        None\n" + 
+        		"    Modifiers: 1\n" + 
+        		"\n" + 
+        		"    Name: printSomething\n" + 
+        		"    Exceptions thrown: \n" + 
+        		"        None\n" + 
+        		"    Parameter types: \n" + 
+        		"        None\n" + 
+        		"    Modifiers: 2\n" + 
+        		"\n" + 
+        		"    Name: run\n" + 
+        		"    Exceptions thrown: \n" + 
+        		"        None\n" + 
+        		"    Parameter types: \n" + 
+        		"        None\n" + 
+        		"    Modifiers: 1\n" + 
+        		"\n" + 
+        		"    Name: toString\n" + 
+        		"    Exceptions thrown: \n" + 
+        		"        None\n" + 
+        		"    Parameter types: \n" + 
+        		"        None\n" + 
+        		"    Modifiers: 1\n" + 
+        		"\n" + 
+        		"    Name: setVal\n" + 
+        		"    Exceptions thrown: \n" + 
+        		"        java.lang.Exception\n" + 
+        		"    Parameter types: \n" + 
+        		"        int\n" + 
+        		"    Modifiers: 1\n" + 
+        		"\n", actualOutput);
+    }
+	
 }
